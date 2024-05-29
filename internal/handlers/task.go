40d9c3982023f04fdbc5708f22497ebe69b24824
atauov <atauov@gmail.com>
2024-05-29 15:39:12 +0500

    handlers

---
+++ internal/handlers/task.go
@@ -0,0 +1,23 @@
+package handlers
+
+import "github.com/labstack/echo/v4"
+
+func (h *Handler) createTask(c *echo.Context) {
+
+}
+
+func (h *Handler) getAllTasks(c *echo.Context) {
+
+}
+
+func (h *Handler) getTask(c *echo.Context) {
+
+}
+
+func (h *Handler) updateTask(c *echo.Context) {
+
+}
+
+func (h *Handler) deleteTask(c *echo.Context) {
+
+}
